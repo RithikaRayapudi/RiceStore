f4d9fcd12dafa23b507423bd6280f0a92ba3951a
RithikaRayapudi <rithikarayapudi@gmail.com>
2025-06-25 12:19:38 +0530

    new

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://ricestore-ewyq.onrender.com/api';
-
+// const API_BASE_URL = 'https://ricestore-ewyq.onrender.com/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 export default API_BASE_URL;
