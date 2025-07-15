18b1340d4d4b163b723caa212ef69967d9cc9ad9
RithikaRayapudi <rithikarayapudi@gmail.com>
2025-07-15 17:45:18 +0530

    new

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,2 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
-if (!API_BASE_URL) {
-  throw new Error("REACT_APP_API_BASE_URL is not defined in environment variables.");
-}
-
 export default API_BASE_URL;
